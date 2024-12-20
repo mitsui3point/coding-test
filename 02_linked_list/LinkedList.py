e93151f110c81258c1a7d5bfac24a3818504c956
mitsui3point <rladbsgh0331@gmail.com>
2024-12-20 15:23:26 +0900

    feat(linkedList): add linkedList insert_at method

--- 02_linked_list/LinkedList.py
+++ 02_linked_list/LinkedList.py
@@ -4,7 +4,7 @@ class LinkedList:
     def __init__(self, head = None):
         self.head = head
     
-    def append(self, value):
+    def insert_back(self, value):
         new_node = Node(value)
         if self.head == None:
             self.head = new_node
@@ -22,6 +22,22 @@ class LinkedList:
             current = current.next
             index += 1
         return current
+        
+    # TODO: insert_at 구현
+    def insert_at(self, idx, value):
+        new_node = Node(value)
+        current = self.head
+        if self.head == None:
+            self.head = new_node
+            return
+        if idx == 0:
+            new_node.next = current
+            self.head = new_node
+            return
+        for _ in range(0, idx - 1):
+            current = current.next
+        new_node.next = current.next
+        current.next = new_node
 
     def __str__(self):
         return f"LinkedList(header={self.head})"
--- 02_linked_list/Test.py
+++ 02_linked_list/Test.py
@@ -3,12 +3,19 @@ from LinkedList import LinkedList  # Node.py에서 Node 클래스를 가져옴
 
 if __name__ == "__main__":
     linkedList = LinkedList()
-    linkedList.append(4)
-    linkedList.append(3)
-    linkedList.append(2)
-    linkedList.append(1)
+    linkedList.insert_back(4)
+    linkedList.insert_back(3)
+    linkedList.insert_back(2)
+    linkedList.insert_back(1)
     print('linkedList.get(0)', linkedList.get(0))
     print('linkedList.get(1)', linkedList.get(1))
     print('linkedList.get(2)', linkedList.get(2))
     print('linkedList.get(3)', linkedList.get(3))
+    linkedList.insert_at(0, 100)
+    print('=======================================')
+    print('linkedList.get(0)', linkedList.get(0))
+    print('linkedList.get(1)', linkedList.get(1))
+    print('linkedList.get(2)', linkedList.get(2))
+    print('linkedList.get(3)', linkedList.get(3))
+    print('linkedList.get(4)', linkedList.get(4))
 
