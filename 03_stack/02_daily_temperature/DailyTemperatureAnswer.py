b2258dec3bbf484ac544182b33c46aaf0d84be0e
mitsui3point <rladbsgh0331@gmail.com>
2024-12-27 17:20:00 +0900

    feat(daily_temperature): remove print DailyTemperature
    739. Daily Temperatures
    https://leetcode.com/problems/daily-temperatures/

--- 03_stack/02_daily_temperature/DailyTemperatureAnswer.py
+++ 03_stack/02_daily_temperature/DailyTemperatureAnswer.py
@@ -5,17 +5,10 @@ class Solution(object):
         :rtype: List[int]
         """
         answer = [0] * len(temperatures)
-        print(answer)
         stack = []
         for cur_day, cur_temp in enumerate(temperatures):
-            print('-----------')
-            print(cur_day, cur_temp)
-            print('stack',stack)
-            print('answer',answer)
             while stack and stack[-1][1] < cur_temp:
-                print('===')
                 prev_day, _ = stack.pop()
-                print(prev_day, _)
                 answer[prev_day] = cur_day - prev_day
             stack.append((cur_day, cur_temp))
         return answer
@@ -23,11 +16,8 @@ class Solution(object):
 if __name__ =='__main__':
     solution = Solution()
     
-    # print(solution.dailyTemperatures([73,74,75,71,69,72,76,73]))
     print(solution.dailyTemperatures([73,74,75,71,69,72,76,73]) == [1,1,4,2,1,1,0,0])
-    # print(solution.dailyTemperatures([30,40,50,60]))
-    # print(solution.dailyTemperatures([30,40,50,60]) == [1,1,1,0])
-    # print(solution.dailyTemperatures([30,60,90]))
-    # print(solution.dailyTemperatures([30,60,90]) == [1,1,0])
+    print(solution.dailyTemperatures([30,40,50,60]) == [1,1,1,0])
+    print(solution.dailyTemperatures([30,60,90]) == [1,1,0])
 
 
