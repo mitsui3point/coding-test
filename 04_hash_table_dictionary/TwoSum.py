3284d3073cecce90417ce5941b07e91038c0fb18
mitsui3point <rladbsgh0331@gmail.com>
2024-12-27 19:22:35 +0900

    feat(hash_table;two_sum): add two_sum apply hash table
    1. Two Sum
    https://leetcode.com/problems/two-sum/description/

---
+++ 04_hash_table_dictionary/TwoSum.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        d = {}
+        for index, value in enumerate(nums):
+            if value in d and (target - value) in d:
+                return [d[value], index]
+            d[value] = index
+            if (target - value) in d and d[value] != d[target - value]:
+                if (d[value] > d[target - value]):
+                    return [d[target - value], d[value]]
+                return [d[value], d[target - value]]
+        return [0, 0]
+    
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.twoSum(nums=[2,7,11,15], target=9) == [0,1])
+    print(solution.twoSum(nums=[3,2,4], target=6) == [1,2])
+    print(solution.twoSum(nums=[3,3], target=6) == [0,1])
