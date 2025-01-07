f1079070c9386abbbc9ca0876574740e7a56f9da
mitsui3point <rladbsgh0331@gmail.com>
2025-01-07 10:25:55 +0900

    feat(graph;dfs): modify dfs

--- 06_graph/01_graph/Dfs.py
+++ 06_graph/01_graph/Dfs.py
@@ -1,33 +1,17 @@
+graph = {
+        "A": ["B", "D", "E"],
+        "B": ["A", "C", "D"],
+        "C": ["B"],
+        "D": ["A", "B"],
+        "E": ["A"],
+    }
 visited = []
 
-
-def dfs(graph, curr_v, visited=[]):
-
-    curr = graph[curr_v]
-    next = None
-    if curr_v is None:
-        return
-    for next_v in curr:
-        # if result :
-        #     return
-        next = graph[next_v]
-        print("-" * 50)
-        print("curr_v", curr_v, ',', curr)
-        print("next_v", next_v, ',', next)
-        print('prev visited', visited)
-        if (curr_v not in visited):
-            visited.append(curr_v)
-            dfs(graph, next_v, visited)
-            continue
-        if (curr_v in visited):
-            continue
-        print('after visited', visited)
-    return visited
-    # print("-"*50)
-    # print("start_v", start_v)
-    # print("v", v)
-    # print("0visited", visited)
-
+def dfs(curr_v):
+    visited.append(curr_v)
+    for next_v in graph[curr_v]:
+        if (next_v not in visited):
+            dfs(next_v)
 
 if __name__ == "__main__":
     graph_data = {
@@ -37,4 +21,5 @@ if __name__ == "__main__":
         "D": ["A", "B"],
         "E": ["A"],
     }
-    print(dfs(graph_data, "A"))
+    dfs("A")
+    print(visited)
